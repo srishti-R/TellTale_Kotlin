a1ad9a8d80ebb4fdac582b2cce256bb19916bf8b
Srishti <s.rohatgi23@gmail.com>
2020-05-28 15:50:29 +0530

    App flow checked.

--- app/src/main/java/com/srishti/telltale/fragments/FirstFragment.kt
+++ app/src/main/java/com/srishti/telltale/fragments/FirstFragment.kt
@@ -7,24 +7,29 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.srishti.telltale.R
-import kotlinx.android.synthetic.main.fragment_second.view.*
+import kotlinx.android.synthetic.main.fragment_first.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class SecondFragment : Fragment() {
+class FirstFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_second, container, false)
-        val opt1 = view.opt_cats
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+        val opt1 = view.opt_doggos
         opt1.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
+            view.findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+        val opt2 = view.opt_virgin
+        opt2.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
         }
         return view
+
     }
 
 }
